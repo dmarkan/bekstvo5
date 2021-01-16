5af81b771b7a96fa5410cd5b327073802b1f603e
Darko Markan <75615049+dmarkan@users.noreply.github.com>
2021-01-16 22:44:51 +0100

    Initial commit

---
+++ java.js
@@ -0,0 +1,10 @@
+function forma() {
+
+var x = document.querySelector("#vrata input").value;
+if(x === "13097762") {
+alert("Vrata su se otključala");
+document.write("KRAJ");
+} else {
+alert("Pogrešna šifra");
+}
+}
